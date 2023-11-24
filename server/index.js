d8e822e3e0005dc736e43879ce7d37f65911cca5
Quezelco1Corplan <104293978+git-dariel@users.noreply.github.com>
2023-11-24 18:42:16 +0800

    adding dotenv file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const getProducts = require("./routers/Connection/apiProducts.js");
 
 const app = express();
